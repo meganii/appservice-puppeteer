b7530176f36a232b8e839903ed2ad05fbd6bfc9b
meganii <yuhei24@gmail.com>
2019-02-26 23:01:38 +0900

    var

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const http = require('http');
+var http = require('http');
 
-const server = http.createServer(function(request, response) {
+var server = http.createServer(function(request, response) {
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.end("Hello World!");
 });
 
-const port = process.env.PORT || 1337;
+var port = process.env.PORT || 1337;
 server.listen(port);
 
 console.log("Server running at http://localhost:%d", port);
